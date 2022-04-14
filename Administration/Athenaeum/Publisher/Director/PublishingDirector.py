c82118f6b17b1681d99e31ca579d3010c31ad203
Technicus <>
2022-04-14 09:38:32 -0500

    Logging seems to work.

--- Administration/Athenaeum/Publisher/Director/PublishingDirector.py
+++ Administration/Athenaeum/Publisher/Director/PublishingDirector.py
@@ -5,13 +5,11 @@
 #from os.path import relpath
 #import subprocess
 #from termcolor import colored
-from os import path, getcwd, chdir
 #from Registry import registry, registry_report, registry_query
+from os import path, getcwd, chdir
 from PublishingManagement import Registry
-#from logging import debug, info, warning, error, basicConfig, getLogger
 from logging import debug, info, warning, error, basicConfig, DEBUG, INFO,\
-    WARNING, ERROR, getLogger
-import logging
+    WARNING, ERROR, getLogger, config
 from sys import path as sysPath
 
 
@@ -61,38 +59,47 @@ def main():
         #.get('project_root')) + '/' + str(
         #''.join(registry.search('publisher.log', 'files')))
         #.lstrip('./'))
+    log_file_config = './Utilities/Maintenance/Logger.ini'
+    config.fileConfig(log_file_config, disable_existing_loggers=False)
+    debug('This message should appear on the console')
+    info('So should this')
+    warning('And this, too')
+    debug('This message should go to the log file')
+    info('So should this')
+    warning('And this, too')
+    error('And non-ASCII stuff, too, like Øresund and Malmö')
 
-    log_publisher = ''.join(registry.search('publisher.log', 'files'))
+    #log_publisher = ''.join(registry.search('publisher.log', 'files'))
 
             #''.join(registry.report('path').get('project_root'))) + '/' \
             #+ str(directory).lstrip('./')
     #print(getcwd())
-    print(log_publisher)
+    #print(log_file_config)
 
-    FORMAT = '%(asctime)s %(message)s'
-    basicConfig(
-        format=FORMAT,
-        filename='log_publisher',
-        filemode='w',
-        encoding='utf-8',
-        level=INFO)
-    logger = getLogger()
+    #FORMAT = '%(asctime)s %(message)s'
+    #basicConfig(
+        #format=FORMAT,
+        #filename='log_publisher',
+        #filemode='w',
+        #encoding='utf-8',
+        #level=INFO)
+    #logger = getLogger()
     #info('[[ {} ]]\n{}>>> [ main() ]\n'
          #.format(path.basename(__file__), indent[0]))
     #info('[ project_path ]\n\t[ {} ]\n'.format(project_path))
     #info('[ project_root ]\n\t[ {} ]\n'.format(project_root))
     #info('[ test ]\n\t[ {} ]\n'.format(project_root))
 
-    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', filename='./Utilities/Data/Log/publisher.log' ,level=logging.DEBUG)
-    logger = logging.getLogger(__name__)
-    logging.debug('This message should appear on the console')
-    logging.info('So should this')
-    logging.warning('And this, too')
+    #logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', filename='./Utilities/Data/Log/publisher.log' ,level=logging.DEBUG)
+    #logger = logging.getLogger(__name__)
+    #logging.debug('This message should appear on the console')
+    #logging.info('So should this')
+    #logging.warning('And this, too')
 
-    logger.debug('This message should go to the log file')
-    logger.info('So should this')
-    logger.warning('And this, too')
-    logger.error('And non-ASCII stuff, too, like Øresund and Malmö')
+    #logger.debug('This message should go to the log file')
+    #logger.info('So should this')
+    #logger.warning('And this, too')
+    #logger.error('And non-ASCII stuff, too, like Øresund and Malmö')
 
 
     #print(f'registry.search()\n  {registry.search()}\n')
